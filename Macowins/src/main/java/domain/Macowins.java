df5fe4e7fc9ee88a1b3e87c75f043a50fce8cc5c
francorojo <49093389+francorojo@users.noreply.github.com>
2020-04-02 00:03:38 -0300

    Update Macowins.java

--- Macowins/src/main/java/domain/Macowins.java
+++ Macowins/src/main/java/domain/Macowins.java
@@ -47,19 +47,12 @@ public class Macowins {
 Requeremientos:
 REQUERIMIENTOS FUNCIONALES: 
 1-Saber precio,estado y tipo de una prenda
-2-Conocer la ganancia diaria de las prendas vendidas (Pregunta al usuario, el recargo por cuotas adicional se incorpora dentro del precio neto?
+2-Conocer la ganancia diaria de las prendas vendidas en un dia (Pregunta al usuario, el recargo por cuotas adicional se incorpora dentro del precio neto?
 O es un beneficio para el banco por brindar el servicio?
 En ese caso desea tener un registro del precio bruto(sin este adicional) y uno neto?
 O desea tener el precio neto final y almacenar el procentaje de ganancia del banco?)
 REQUERIMIENTOS NO FUNCIONALES: No encontra ninguno
 Aclaraciones:
-///El codigo tiene errores de compilacion ya que no conozco el lenguaje en su profundidad. Pero creo que se entiende en su
-mayoria. Tuve problemas con el tipo Date y como el funcionamiento con los statics en las clases.
-///Los pagos en Efectivo se anotan con cuota=1.
-///Descarte la codificacion de todos los getters y setters que no se hacen uso.
-/// El metodo hayStock lo inclui para hacer mas razonable el contexto del codigo y validar la venta. Solo se asume
-la cantidad de stock y no el tipo de prenda en stock.
-///Considere Macowins como una clase, de esta forma se podria asumir que cada clase Macowins es un
-local de Macowins. Capaz, de momento no seria necesario pero a futuro probablemente seria conveniente
-si se implenta el sistema a una escala mayor.
+Concidere que seria recomendable crear la clase Macowins y ponerle una id_sucursal, sino hubiera creado un objeto conocido.
+El tipo de prenda no lo concidere importante para hacer una clase, solo un int que se relacione con cada tipo.
 */
